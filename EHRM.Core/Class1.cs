c7ff74fe947c67fb614559302588ac07622745f2
Waseem Ahmad <ag2976@yale.edu>
2024-11-25 13:44:59 +0530

    Remaning Changes from Waseem

---
+++ EHRM.Core/Class1.cs
@@ -0,0 +1,7 @@
+﻿namespace EHRM.Core
+{
+    public class Class1
+    {
+
+    }
+}
---
+++ EHRM.Core/IUnitOfWork.cs
@@ -0,0 +1,17 @@
+﻿using EHRM.DAL.Database;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EHRM.Core
+{
+    public interface IUnitOfWork : IDisposable
+    {
+        IGenericRepository<Role> RoleRepository { get; }
+        Task SaveAsync();
+    }
+
+}
