da96212aa31204443f2eab6b502a4c0f927c514c
RAPS-Saksham <RAPS-Saksham@DESKTOP-PT8NET5>
2024-12-17 18:21:39 +0530

    static dashboard is created

---
+++ EHRM.Web/Controllers/DashboardController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace EHRM.Web.Controllers
+{
+    public class DashboardController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Dashboard()
+        {
+            return View();
+        }
+        public IActionResult Manager()
+        {
+            return View();
+        }
+        public IActionResult User()
+        {
+            return View();
+        }
+    }
+}
