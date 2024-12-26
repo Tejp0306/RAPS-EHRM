9aab957bd09d319d8e421513ccc491b216a911e2
RAPS-Saksham <RAPS-Saksham@DESKTOP-PT8NET5>
2024-12-26 19:54:12 +0530

    changes completedin self and HR

---
+++ EHRM.Web/Controllers/SelfController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace EHRM.Web.Controllers
+{
+    public class SelfController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult SelfPortal()
+        {
+            return View();
+        }
+
+        public IActionResult Profile()
+        {
+            return View();
+        }
+    }
+}
