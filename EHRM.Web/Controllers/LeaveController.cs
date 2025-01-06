71f541c04817043957368e68dd3b92e4ee731ec5
RAPS-Saksham <RAPS-Saksham@DESKTOP-PT8NET5>
2025-01-06 15:35:34 +0530

    Leave

---
+++ EHRM.Web/Controllers/LeaveController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace EHRM.Web.Controllers
+{
+    public class LeaveController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult AddLeaveType()
+        {
+            return View();
+        }
+        public IActionResult LeaveApply()
+        {
+            return View();
+        }
+    }
+}
