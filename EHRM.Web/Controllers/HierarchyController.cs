eadc0161275a17dd0d8dd7633deb6c245beb7519
Arjunraps <167761644+Arjunraps@users.noreply.github.com>
2025-02-25 18:35:21 +0530

    Hierarchy Changes (#58)
    Co-authored-by: arjun0bot <arjunsingh8545999@gmail.com>

---
+++ EHRM.Web/Controllers/HierarchyController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using EHRM.DAL.Database;
+using EHRM.ViewModel.Hierarchy;
+using System.Collections.Generic;
+using Microsoft.Data.SqlClient;
+using EHRM.ServiceLayer.Hierarchy;
+using System.Text.Json;
+using Microsoft.EntityFrameworkCore.Metadata.Internal; // For JsonPropertyName
+
+
+
+public class HierarchyController : Controller
+{
+    private IHierarchyService _hierarchy;
+
+    public HierarchyController(IHierarchyService hierarchy)
+    {
+        _hierarchy = hierarchy;
+    }
+
+    public IActionResult HierarchyView()
+    {
+        return View();
+    }
+
+    public string ShowHierarchy()
+    {
+        List<HierarchyViewModel> employees = _hierarchy.GetEmployeesTreeForHierarchy();
+        var jsonOptions = new JsonSerializerOptions
+        {
+            PropertyNamingPolicy = JsonNamingPolicy.CamelCase // Crucial: Use camelCase
+        };
+
+        return JsonSerializer.Serialize(employees, jsonOptions);
+      
+
+    }
+}
